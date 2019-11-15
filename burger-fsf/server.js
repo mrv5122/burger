8a449cbbc946c2c7b08f4c12b66deaf425b0b4ae
Manuel R <mannyvazqsan1@icloud.com>
2019-11-15 16:05:30 -0500

    fixed server.js

--- burger-fsf/server.js
+++ burger-fsf/server.js
@@ -1,20 +1,29 @@
 var express = require('express');
+
+// handlebars
 var exphbs = require("express-handlebars");
-// TODO: Import your route files from `route/`
+app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+app.set("view engine", "handlebars");
 
 // Initialize the app and create a port
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 4000;
 
 // Set up body parsing, static, and route middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
-app.set("view engine", "handlebars");
+app.use(express.static("public"));
 
+
+//parse app body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+// import routes
 var routes = require("./controllers/burgers_controllers.js")
 app.use(routes);
+
+
 // Start the server on the port
 app.listen(PORT, function() {
-  console.log('Listening on PORT: ' + PORT);
+  console.log('Server Listening on PORT: ' + PORT);
 });
